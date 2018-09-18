1dfd757da69ea91b2bcf1ac6ccea2c1892d0a8d5
iflgit <6567967@gmail.com>
2018-09-18 12:34:58 +0300

    homework for lesson 04

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -1,8 +1,12 @@
+__author__ = 'Nekhamchin Anatoly'
 # Задание-1:
 # Вывести символы в нижнем регистре, которые находятся вокруг
 # 1 или более символов в верхнем регистре.
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
+print("1:")
+
+import re
 
 line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
@@ -20,6 +24,25 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
+pattern = "[A-Z]*([a-z]+)[A-Z]*"
+print(re.findall(pattern, line))
+_res = []
+_current = ''
+_waslower = 0  # 1 - lower ok, 0 - upper
+for char in line:
+    if char.islower() and _waslower:
+        _current += char
+    if char.isupper() and _waslower and len(_current):
+        _res.append(_current)
+        _current = ''
+#        _waslower = False
+    if char.islower() and not _waslower:
+        _current = char
+        _waslower = True
+#    if char.isupper() and not _waslower:
+if len(_current):
+    _res.append(_current)
+print(_res)
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
@@ -28,7 +51,7 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
 # "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
 # нужно получить список строк: ['AY', 'NOGI', 'P']
 # Решить задачу двумя способами: с помощью re и без.
-
+print("2:")
 line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'NOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewV'\
        'fzKTUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSA'\
@@ -45,9 +68,58 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+print(re.findall(r"[a-z]{2}([A-Z]+)[A-Z]{2}", line_2))
+_res = []
+_current = ''
+_state = 1  #0-must be lower 1-find lower 2-find start 3-start
+for char in line_2:
+    if char.islower() and _state == 1:
+        _state = 0
+    elif char.islower() and _state == 0:
+        _state = 2
+    elif char.isupper() and _state == 0:
+        _state = 1
+    elif char.isupper() and _state == 2:
+        _state = 3
+        _current = char
+    elif char.isupper() and _state == 3:
+        _current += char
+    elif char.islower() and _state == 3:
+        if len(_current) > 2:
+            _res.append(_current[:-2])
+        _state = 0
+        _current = ''
+
+if len(_current) > 2:
+    _res.append(_current[:-2])
+print(_res)
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+print("3:")
+
+import random
+
+filename = "datafile.txt"
+
+with open(filename, "wt", encoding="UTF-8") as datafile:
+    # print("".join([str(random.randint(0,9)) for _ in range(2500)]))
+    datafile.write("".join([str(random.randint(0,9)) for _ in range(2500)]))
+with open(filename, "rt", encoding="UTF-8") as datafile:
+    _data = datafile.read()
+    print(_data)
+    _longest = ''
+    _current = ''
+    for _i, char in enumerate(_data[:-1]):
+        if len(_current) > 0 and _current[0] == char:
+            _current += char
+        else:
+            _current = char
+        if len(_longest) < len(_current):
+            _longest = _current
+    print(_longest)
+
